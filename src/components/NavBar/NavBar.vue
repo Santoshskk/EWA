1100ad187116cc610b77d8affcc9bf0b46e4a60a
Romello <romello.ten.broeke@hva.nl>
2023-09-27 23:11:30 +0200

    NavBar purple and active text almost working

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,47 +1,40 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-navbar-color navbar-item-text">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+<!--        NavbarItems-->
+        <NavBarItem item-text="Home" route="/"/>
+        <NavBarItem item-text="About us" route="/about_us"/>
+<!--        Drop down admin dashboard menu -->
+        <NavBarDropdown/>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+        <button class="btn btn-outline-success" type="submit">Login</button>
     </div>
   </div>
 </nav>
 </template>
 <script>
+import NavBarItem from '@/components/NavBar/NavBarItem'
+import NavBarDropdown from '@/components/NavBar/NavBarDropdown'
+
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  components: { NavBarDropdown, NavBarItem }
 }
 </script>
-
 <style>
+.bg-navbar-color {
+  background: purple;
+}
 
+.navbar-item-text {
+  color: azure;
+  font-size: 20px;
+}
 </style>
---
+++ src/components/NavBar/NavBarItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="nav-item">
+    <router-link class="nav-link" exact-active-class="bg-navbar-active" :to='route'>{{ itemText }}</router-link>
+  </li>
+</template>
+<script>
+export default {
+  name: 'NavBarItem',
+  props: ['itemText', 'route']
+}
+</script>
+<style>
+.bg-navbar-active {
+  color: dodgerblue;
+}
+
+</style>
