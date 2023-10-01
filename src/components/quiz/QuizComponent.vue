d9774605cb44c0990da027bb2bf46f7d9a585bf3
Marco de Boer <128293216+GitMarcoo@users.noreply.github.com>
2023-10-01 11:51:19 +0200

    basic setup for quiz

---
+++ src/components/quiz/QuizComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Quiz</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'QuizComponent',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
