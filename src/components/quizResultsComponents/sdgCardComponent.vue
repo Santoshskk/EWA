de86e61058e0375101160096da9d8707e4cf45d2
Romello <romello.ten.broeke@hva.nl>
2023-10-04 22:10:42 +0200

    Started with the results of the quiz

---
+++ src/components/quizResultsComponents/sdgCardComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card m-1" style="width: 18rem;">
+    <img class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sdgCardComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/quizResultsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class="mx-0">These are your top 3 sdgs!</h2>
+  <section class="container mx-5">
+    <div class="row">
+      <sdg-card-component/>
+      <sdg-card-component/>
+      <sdg-card-component/>
+    </div>
+  </section>
+</template>
+
+<script>
+import SdgCardComponent from '@/components/quizResultsComponents/sdgCardComponent'
+export default {
+  name: 'quizResultsView',
+  components: { SdgCardComponent }
+}
+</script>
+
+<style scoped>
+
+</style>
