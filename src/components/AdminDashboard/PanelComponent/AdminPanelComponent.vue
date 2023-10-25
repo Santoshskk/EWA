653b0741e8da907ac4dbcabe78884429c3744214
Romello <romello.ten.broeke@hva.nl>
2023-10-26 00:57:18 +0200

    added panel implentation

---
+++ src/components/AdminDashboard/PanelComponent/AdminPanelComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card bg-light m-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="imgLink" class="card-img" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ title }}</h5>
+          <p class="card-text">{{ description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminPanelComponent',
+  props: ['title', 'description', 'imgLink']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/AdminDashboardView.vue
+++ src/views/AdminDashboardView.vue
@@ -16,13 +16,22 @@
     <div class="card-body">
       <h5 class="card-title">Welcome to the admin dashboard</h5>
       <p class="card-text">On this page you will find all the activities an admin can do and a small explanation per activity</p>
+<!--      Panel holder -->
+      <div class="row">
+        <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
+        <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
+        <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import AdminPanelComponent from '@/components/AdminDashboard/PanelComponent/AdminPanelComponent'
 export default {
-  name: 'AdminDashboardView'
+  name: 'AdminDashboardView',
+  components: { AdminPanelComponent }
 }
 </script>
 
