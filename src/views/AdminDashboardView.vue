b8e50e00308db1c21550f0e121367142b04d508a
Romello <romello.ten.broeke@hva.nl>
2023-10-26 00:37:39 +0200

    Changed admin navbar Item for clarity

--- src/views/AdminDashboardView.vue
+++ src/views/AdminDashboardView.vue
@@ -3,17 +3,19 @@
     <div class="card-header">
       <ul class="nav nav-tabs card-header-tabs">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
+          <router-link class="nav-link " active-class="active" to='/admin_dashboard'>Intro</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link text-black" href="#">Users</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-black" href="#">Content</a>
         </li>
       </ul>
     </div>
     <div class="card-body">
-      <h5 class="card-title">Special title treatment</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">Welcome to the admin dashboard</h5>
+      <p class="card-text">On this page you will find all the activities an admin can do and a small explanation per activity</p>
     </div>
   </div>
 </template>
