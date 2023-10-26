08610d35d2eacd501d3e38afd45fde8deda1f79a
Romello <romello.ten.broeke@hva.nl>
2023-10-26 11:26:19 +0200

    Added router to AdminDashboardView

---
+++ src/components/AdminDashboard/IntroComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
+    <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
+    <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
+  </div>
+</template>
+<script>
+import AdminPanelComponent from '@/components/AdminDashboard/PanelComponent/AdminPanelComponent'
+
+export default {
+  name: 'IntroComponent',
+  components: { AdminPanelComponent }
+}
+</script>
--- src/views/AdminDashboardView.vue
+++ src/views/AdminDashboardView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="card text-center">
     <div class="card-header">
+<!--      Dashboard navbar-->
       <ul class="nav nav-tabs card-header-tabs">
         <li class="nav-item">
-          <router-link class="nav-link " active-class="active" to='/admin_dashboard'>Intro</router-link>
+          <router-link class="nav-link " active-class="active" to='intro'>Intro</router-link>
         </li>
         <li class="nav-item">
           <a class="nav-link text-black" href="#">Users</a>
@@ -16,25 +17,15 @@
     <div class="card-body">
       <h5 class="card-title">Welcome to the admin dashboard</h5>
       <p class="card-text">On this page you will find all the activities an admin can do and a small explanation per activity</p>
-<!--      Panel holder -->
-      <div class="row">
-        <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
-        <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
-        <AdminPanelComponent title="Users" description="Lets you edit users" img-link=""/>
-      </div>
-
+<!--      Loads each section of the admin dashboard as clicked in the admin navbar. -->
+      <router-view/>
     </div>
   </div>
 </template>
 
 <script>
-import AdminPanelComponent from '@/components/AdminDashboard/PanelComponent/AdminPanelComponent'
+
 export default {
-  name: 'AdminDashboardView',
-  components: { AdminPanelComponent }
+  name: 'AdminDashboardView'
 }
 </script>
-
-<style scoped>
-
-</style>
