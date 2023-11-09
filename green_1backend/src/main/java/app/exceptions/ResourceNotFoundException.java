63fd1f49d645871079fb3361bca95f304e8b78d1
Romello <romello.ten.broeke@hva.nl>
2023-11-09 23:50:47 +0100

    Better error handling and concept will now be updated appropriately

--- green_1backend/src/main/java/app/exceptions/ResourceNotFoundException.java
+++ green_1backend/src/main/java/app/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,11 @@
 package app.exceptions;
 
-public class ResourceNotFoundException extends Exception{
-    public ResourceNotFoundException(String s) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
--- green_1backend/src/main/java/app/rest/PageContentController.java
+++ green_1backend/src/main/java/app/rest/PageContentController.java
@@ -22,13 +22,48 @@ public class PageContentController {
 
 
 // Gets all the content for one page per the Page ID
-    @GetMapping("/page/{pageId}/all")
+    @GetMapping(path = "/page/{pageId}/all")
     public ResponseEntity<List<PageContent>> getAllContentByPageId(@PathVariable(value = "pageId") Long pageId) throws ResourceNotFoundException {
         if (!pageRepository.existsById(Math.toIntExact(pageId))) {
             throw new ResourceNotFoundException("Not found Page with id = " + pageId);
         }
         List<PageContent> pageContents = pageContentRepository.findAllByFkPage_PageId(pageId);
         return new ResponseEntity<>(pageContents, HttpStatus.OK);
+    }
+
+    /**
+     *
+     * @param contentId id of the content to be changed
+     * @param contentConcept is the parameter that says if only the concept needs to be changed.
+     * @param pageContent is all the content that needs to be saved.
+     * @return if the data is saved succesfully or not.
+     * @author Romello ten Broeke
+     */
+    @PostMapping("/{contentId}")
+    public ResponseEntity<PageContent> updateContent(
+            @PathVariable(value = "contentId") Long contentId,
+            @RequestParam(value = "contentConcept", required = false) String contentConcept,
+            @RequestBody PageContent pageContent
+    ) throws ResourceNotFoundException {
+        PageContent existingPageContent = pageContentRepository.findById(Math.toIntExact(contentId)).orElse(null);
+
+        if (existingPageContent != null) {
+            // Check if contentConcept parameter is provided in the URL
+            if (contentConcept != null) {
+                existingPageContent.setContentConcept(contentConcept);
+            } else {
+                // Update other fields based on the request body
+                existingPageContent.setContentTitle(pageContent.getContentTitle());
+                existingPageContent.setContentDutch(pageContent.getContentDutch());
+                existingPageContent.setContentEnglish(pageContent.getContentEnglish());
+                existingPageContent.setContentConcept(pageContent.getContentConcept());
+            }
+
+            PageContent updatedPageContent = pageContentRepository.save(existingPageContent);
+            return new ResponseEntity<>(updatedPageContent, HttpStatus.OK);
+        } else {
+            throw new ResourceNotFoundException("Not found content with id = " + contentId);
+        }
     }
 
 
