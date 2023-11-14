0c6cc35b44621420ddc07b801c3628de36a343ef
Marco <marco.de.boer3@hva.nl>
2023-11-14 13:14:28 +0100

    small fix on delete button option

--- src/components/adminquiz/QuizBuilderMultipleChoice.vue
+++ src/components/adminquiz/QuizBuilderMultipleChoice.vue
@@ -46,7 +46,7 @@
                   <label class="justify-content-start h5 m-auto col" :for="questionClone.answerLimit">Amount of options can select:</label>
                   <input class="col mx-2 answerLimit" v-model="questionClone.answerLimit" :class="{ 'red-border': !questionClone.answerLimitIsValid }" />
             </div>
-            <div class="m-auto row optionListBuilder" v-for="option in questionClone.options" :key="option.id">
+            <div class="m-auto row optionListBuilder" v-for="(option,key) in questionClone.options" :key="option.id">
               <div class="m-auto row multipleChoiceBuilderQuestionButton">
                 <div class="col-9 quizButtonSection p-0 justify-content-center m-auto mx-0">
                   <label class="justify-content-start h5 m-auto" :for="option.option">Option:</label>
@@ -62,12 +62,12 @@
                   </div>
                   <div class="col-1">
                     <div class="margin31"></div>
-                    <button class="questionDeleteButton justify-content-center m-auto" @mouseenter="deleteButtonHoverOption[option.id] = true" @mouseleave="deleteButtonHoverOption[option.id] = false" @click="deleteOption(option)">
-                      <svg v-if="!deleteButtonHoverOption[option.id]" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="red" class="bi bi-trash" viewBox="0 0 20 20">
+                    <button class="questionDeleteButton justify-content-center m-auto" @mouseenter="deleteButtonHoverOption[key] = true" @mouseleave="deleteButtonHoverOption[key] = false" @click="deleteOption(option)">
+                      <svg v-if="!deleteButtonHoverOption[key]" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="red" class="bi bi-trash" viewBox="0 0 20 20">
                           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
                       </svg>
-                      <svg v-if="deleteButtonHoverOption[option.id]"  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="red" class="bi bi-trash-fill" viewBox="0 0 20 20">
+                      <svg v-if="deleteButtonHoverOption[key]"  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="red" class="bi bi-trash-fill" viewBox="0 0 20 20">
                           <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                       </svg>
                     </button>
